89cd3c327d193803181c12d8097812fca10f9026
joamag <joamag@gmail.com>
2012-11-13 19:25:12 +0000

    first commit

---
+++ cameo/cameo.h
@@ -0,0 +1,14 @@
+//
+//  cameo.h
+//  cameo
+//
+//  Created by Administrator on 11/13/12.
+//  Copyright (c) 2012 Hive Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cameo : NSObject {
+}
+
+@end
---
+++ cameo/cameo.m
@@ -0,0 +1,13 @@
+//
+//  cameo.m
+//  cameo
+//
+//  Created by Administrator on 11/13/12.
+//  Copyright (c) 2012 Hive Solutions. All rights reserved.
+//
+
+#import "cameo.h"
+
+@implementation cameo
+
+@end
