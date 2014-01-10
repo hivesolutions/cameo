ca2f4c7325af8c1af28442ad54965405fa664861
joamag <joamag@gmail.com>
2014-01-10 12:21:07 +0000

    new string util added to cameo

---
+++ src/classes/HMString.h
@@ -0,0 +1,13 @@
+//
+//  HMString.h
+//  cameo
+//
+//  Created by João Magalhães on 10/01/14.
+//  Copyright (c) 2014 Hive Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMString : NSObject
+
+@end
---
+++ src/classes/HMString.m
@@ -0,0 +1,13 @@
+//
+//  HMString.m
+//  cameo
+//
+//  Created by João Magalhães on 10/01/14.
+//  Copyright (c) 2014 Hive Solutions. All rights reserved.
+//
+
+#import "HMString.h"
+
+@implementation HMString
+
+@end
