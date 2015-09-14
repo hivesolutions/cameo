9982e88ae5b37abc7c63d6ef9723fef1cc4783b2
joamag <joamag@gmail.com>
2015-09-14 16:50:34 +0100

    new color util, fixes #5

---
+++ src/classes/UIColor+HMColorUtil.m
@@ -0,0 +1,52 @@
+// Hive Cameo Framework
+// Copyright (C) 2008-2015 Hive Solutions Lda.
+//
+// This file is part of Hive Cameo Framework.
+//
+// Hive Cameo Framework is free software: you can redistribute it and/or modify
+// it under the terms of the Apache License as published by the Apache
+// Foundation, either version 2.0 of the License, or (at your option) any
+// later version.
+//
+// Hive Cameo Framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// Apache License for more details.
+//
+// You should have received a copy of the Apache License along with
+// Hive Cameo Framework. If not, see <http://www.apache.org/licenses/>.
+
+// __author__    = João Magalhães <joamag@hive.pt>
+// __version__   = 1.0.0
+// __revision__  = $LastChangedRevision$
+// __date__      = $LastChangedDate$
+// __copyright__ = Copyright (c) 2008-2015 Hive Solutions Lda.
+// __license__   = Apache License, Version 2.0
+
+#import "UIColor+HMColorUtil.h"
+
+@implementation UIColor(HMColorUtil)
+
++ (UIColor *)colorWithHex:(NSInteger)hex {
+    NSInteger red = (hex & 0xFF0000) >> 16;
+    NSInteger green = (hex & 0xFF00) >> 8;
+    NSInteger blue = (hex & 0xFF);
+    UIColor *color = [UIColor colorWithRed:red/255.0
+                                     green:green/255.0
+                                      blue:blue/255.0
+                                     alpha:1.0];
+    return color;
+}
+
+- (NSString *)hexString {
+    CGFloat red, green, blue, alpha;
+    [self getRed:&red green:&green blue:&blue alpha:&alpha];
+    NSInteger redI = (NSUInteger) (255.0 * red);
+    NSInteger greenI = (NSUInteger) (255.0 * green);
+    NSInteger blueI = (NSUInteger) (255.0 * blue);
+    NSString *hexString = [NSString stringWithFormat:@"%02lx%02lx%02lx",
+                           (long) redI, (long) greenI, (long) blueI];
+    return hexString;
+}
+
+@end
