2c7e5a7a2017009ad9423169a5a67f83e3b3f500
joamag <joamag@gmail.com>
2017-11-18 15:42:10 +0000

    initial cameo framework support

---
+++ cameo/cameo.h
@@ -0,0 +1,19 @@
+//
+//  cameo.h
+//  cameo
+//
+//  Created by João Magalhães on 18/11/2017.
+//  Copyright © 2017 Hive Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cameo.
+FOUNDATION_EXPORT double cameoVersionNumber;
+
+//! Project version string for cameo.
+FOUNDATION_EXPORT const unsigned char cameoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cameo/PublicHeader.h>
+
+
